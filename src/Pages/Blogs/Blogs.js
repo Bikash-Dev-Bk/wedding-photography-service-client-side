ea1fede2d1901f1823b0f8e76a295abaaa2f923c
bikash-dev <bikash.dev.bk@gmail.com>
2022-11-09 03:35:12 +0600

    my-reviews add-service blogs page and route added, private route added

---
+++ src/Pages/Blogs/Blogs.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Blogs = () => {
+    return (
+        <div>
+            <h3 className='text-center my-10'>this is Blogs</h3>
+        </div>
+    );
+};
+
+export default Blogs;
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -7,6 +7,9 @@ const Header = () => {
 
     const menuItems = <>
         <li className="font-semibold"><Link to='/'>Home</Link></li>
+        <li className="font-semibold"><Link to='/blogs'>Blogs</Link></li>
+        <li className="font-semibold"><Link to='/myreviews'>My Reviews</Link></li>
+        <li className="font-semibold"><Link to='/addservice'>Add Service</Link></li>
         <li className="font-semibold"><Link to='/login'>Login</Link></li>
         <li className="font-semibold"><Link to='/register'>Register</Link></li>
     </>
