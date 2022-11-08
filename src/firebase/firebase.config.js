e5fc9b6ddcfedaabb234634184b70b00fb2dad85
bikash-dev <bikash.dev.bk@gmail.com>
2022-11-08 17:45:16 +0600

    firebase react-router-dom tailwind daisyUI install and basic forder structure

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAL9t04hm8BjcIh4mEcWOjficW_ib82Pm0",
+  authDomain: "wedding-photography-d2a13.firebaseapp.com",
+  projectId: "wedding-photography-d2a13",
+  storageBucket: "wedding-photography-d2a13.appspot.com",
+  messagingSenderId: "913032301527",
+  appId: "1:913032301527:web:6756e435b6fe22d4f59992"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
