c89ab166fa92791f1f6b08bae280008fcd02e889
bikash-dev <bikash.dev.bk@gmail.com>
2022-11-12 00:02:06 +0600

    react-photo-view added in gallery

--- src/Pages/Home/PhotoGallery/Gallery.js
+++ src/Pages/Home/PhotoGallery/Gallery.js
@@ -1,12 +1,15 @@
 import React from "react";
+import { PhotoProvider, PhotoView } from "react-photo-view";
 
 const Gallery = ({ photo }) => {
   const  {image}  = photo;
   return (
     <div className="card card-compact w-96  bg-base-100 shadow-xl">
-      <figure >
-        <img className="p-5 w-full" src={image} alt="" />
-      </figure>
+      <PhotoProvider>
+        <PhotoView src={image}>
+          <img className="p-5 w-full" src={image} alt="" />
+        </PhotoView>
+      </PhotoProvider>
     </div>
   );
 };
